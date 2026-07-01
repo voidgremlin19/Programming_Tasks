0d70c457db6e84d023ec0f53cdfb150ac144f1c8
Sakshi D <dhatraksakshi1@gmail.com>
2026-07-01 19:19:15 +0530

    Fix boolean initialization and condition checks

--- set_matrix_zeroes.py
+++ set_matrix_zeroes.py
@@ -1,14 +1,17 @@
 # Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0. You must do it in place.
 
-
 class Solution:
     def setZeroes(self, matrix):
-        m=len(matrix)
-        n=len(matrix[0])
-        rows=[false]*m
-        columns=[false]*n
-
-        for i in range (m):
+        m = len(matrix)
+        n = len(matrix[0])
+        rows = [False] * m
+        cols = [False] * n        
+        for i in range(m):
             for j in range(n):
-                if rows[i] or columns[j]:
-                    matrix[i][j]==0
+                if matrix[i][j] == 0:
+                    rows[i] = True
+                    cols[j] = True        
+        for i in range(m):
+            for j in range(n):
+                if rows[i] or cols[j]:
+                    matrix[i][j] = 0
