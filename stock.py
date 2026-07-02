20ffd3933e1644b390b6aaa77ccc6bc97713e8e4
Sakshi D <dhatraksakshi1@gmail.com>
2026-07-02 19:17:56 +0530

    Add maxProfit method to calculate stock trading profit
    Implement maxProfit method to calculate maximum stock profit.

---
+++ stock.py
@@ -0,0 +1,23 @@
+'''
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+'''
+
+class Solution:
+    def maxProfit(self, prices):
+        min_price = prices[0]
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+
+            profit = price - min_price
+
+            if profit > max_profit:
+                max_profit = profit
+
+        return max_profit
